64c7ce3faab897d59747e3cc291af7a6b6afd8bf
Oleg 'livelace' Popov <o.popov@livelace.ru>
2021-08-31 13:05:17 +0300

    README.md

--- showcase/smart-queue/bootstrap.sh
+++ showcase/smart-queue/bootstrap.sh
@@ -3,6 +3,9 @@
 CASC_JENKINS_CONFIG="/var/jenkins_home/showcase/smart-queue/iac/jenkins.yaml"
 JAVA_OPTS="-Xmx2048m -Djava.awt.headless=true -Djenkins.model.Jenkins.crumbIssuerProxyCompatibility=true -Dpermissive-script-security.enabled=no_security -Dorg.jenkinsci.main.modules.sshd.SSHD.idle-timeout=0 -Djenkins.install.runSetupWizard=false"
 
+# Jenkins user must have access rights for these folders.
+chmod 777 -R "./config" "./scriptler"
+
 docker run -ti --rm \
   --env CASC_JENKINS_CONFIG="$CASC_JENKINS_CONFIG" \
   --env JAVA_OPTS="$JAVA_OPTS" \
